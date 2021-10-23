9f48e1e4931fb2479a9c66d0c089f1bd70333b7c
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-10-23 12:47:31 +0200

    Update redirect URI

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,8 +2,8 @@ import { DISCORD_API_ROUTES } from '../utils/enum';
 
 export function isProd () {
     if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-        return DISCORD_API_ROUTES.OAUTH2_REDIRECT;
+        return `https://paanya.moe:${process.env.PORT}${DISCORD_API_ROUTES.OAUTH2_REDIRECT}`;
     } else {
-        return DISCORD_API_ROUTES.OAUTH2_REDIRECT_DEV;
+        return `http://localhost:3000${DISCORD_API_ROUTES.OAUTH2_REDIRECT}`
     }
 }
