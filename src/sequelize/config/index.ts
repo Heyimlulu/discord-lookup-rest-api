0574f5319e64c57bc84ff73ed9947ea54b30fcae
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2023-01-07 10:41:55 +0100

    Add database process env

--- src/sequelize/config/index.ts
+++ src/sequelize/config/index.ts
@@ -7,10 +7,10 @@ export const development: any = {
 };
 
 export const production: any = {
-  database: "discordlookupdb",
-  username: "root",
-  password: "",
-  host: "127.0.0.1",
-  dialect: "mysql",
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: "postgres",
   logging: false
 };
