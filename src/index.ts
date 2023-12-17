1737bb9d485c95073484ef2f24d55e994ca1d53c
Lucas <lucas.rouillermonay@gmail.com>
2023-12-17 20:25:48 +0100

    🦺 Add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import http from 'http';
-import express, {Express, NextFunction, Request, Response} from 'express';
-import morgan from 'morgan';
-import routes from './routes';
-import { initDb } from './sequelize/sequelize';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-import { getEnvironmentBaseUrl } from './utils/environment';
+import http from "http";
+import express, { Express, NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import routes from "./routes";
+import rateLimit from "express-rate-limit";
+import { initDb } from "./sequelize/sequelize";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import { getEnvironmentBaseUrl } from "./utils/environment";
 
 const router: Express = express();
 
@@ -15,9 +16,30 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'developme
     initDb();
 }
 */
+const excludedDomains = ["discord.name", "localhost"];
+const apiLimiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 10, // limit each IP to 10 requests per windowMs
+  message: "Too many requests from this IP, please try again after 2 minutes",
+  handler: (req: Request, res: Response) => {
+    return res.status(429).json({
+      status: 429,
+      message:
+        "Too many requests from this IP, please try again after 2 minutes",
+    });
+  },
+  skip: (req: Request, res: Response) => {
+    const requestDomain = req.hostname;
+    console.log(requestDomain);
 
+    return excludedDomains.includes(requestDomain);
+  },
+});
+
+// Rate limiter
+router.use(apiLimiter);
 // Logging
-router.use(morgan('dev'));
+router.use(morgan("dev"));
 // Parse the request
 router.use(express.urlencoded({ extended: false }));
 // Takes care of JSON data
@@ -25,58 +47,64 @@ router.use(express.json());
 
 // Rules
 router.use((req: Request, res: Response, next: NextFunction) => {
-    // set the CORS policy
-    res.header('Access-Control-Allow-Origin', '*');
-    // set the CORS headers
-    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
-    // set the CORS method headers
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT,POST,GET');
-        return res.status(200).json({});
-    }
-    next();
+  // set the CORS policy
+  res.header("Access-Control-Allow-Origin", "*");
+  // set the CORS headers
+  res.header(
+    "Access-Control-Allow-Headers",
+    "origin, X-Requested-With,Content-Type,Accept, Authorization"
+  );
+  // set the CORS method headers
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
-router.get('/', (_req, res) => {
-    res.redirect('/api-docs');
+router.get("/", (_req, res) => {
+  res.redirect("/api-docs");
 });
 
-router.use('/static', express.static('./public/assets/flags'));
+router.use("/static", express.static("./public/assets/flags"));
 
-const environment = process.env.NODE_ENV || 'development';
-const port = process.env.PORT || '8080';
+const environment = process.env.NODE_ENV || "development";
+const port = process.env.PORT || "8080";
 const serverUrl = getEnvironmentBaseUrl();
 
 const options = {
-    swaggerDefinition: {
-      info: {
-        version: 'v1.0.0',
-        title: 'Discord Lookup Rest API',
-        description: 'An API to search for a Discord User or Bot by his snowflake ID.',
-      },
-      servers: [
-        {
-            url: serverUrl,
-            description: `${environment} server`,
-        },
-    ],
+  swaggerDefinition: {
+    info: {
+      version: "v1.0.0",
+      title: "Discord Lookup Rest API",
+      description:
+        "An API to search for a Discord User or Bot by his snowflake ID.",
     },
-    apis: ['./swagger.json'],
-  };
+    servers: [
+      {
+        url: serverUrl,
+        description: `${environment} server`,
+      },
+    ],
+  },
+  apis: ["./swagger.json"],
+};
 
 const specs = swaggerJSDoc(options);
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-router.use('/', routes);
+router.use("/", routes);
 
 // Error handling
 router.use((req: Request, res: Response) => {
-    const error = new Error('I think you got lost');
-    return res.status(404).json({
-        message: error.message
-    });
+  const error = new Error("I think you got lost");
+  return res.status(404).json({
+    message: error.message,
+  });
 });
 
 // Server
 const httpServer = http.createServer(router);
-httpServer.listen(port, () => console.log(`The server is running at ${serverUrl}`));
+httpServer.listen(port, () =>
+  console.log(`The server is running at ${serverUrl}`)
+);
