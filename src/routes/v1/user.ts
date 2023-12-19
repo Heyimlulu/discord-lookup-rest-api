5af0ef95713d62098f8c4f3d469b10f573d49126
Lucas <lucas.rouillermonay@gmail.com>
2023-12-19 19:12:32 +0100

    Remove swagger comments

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -3,13 +3,6 @@ import DiscordUserController  from '../../controllers/v1/DiscordUserController';
 
 const router = Router();
 
-/**
- * @swagger
- * /user/{id}:
- *  get:
- *    summary: Get a user by ID
- *    description: Get a user by ID
- */
 router.get('/user/lookup/:id', async (req, res) => {
     const controller = new DiscordUserController();
     const response = await controller.getUserByID(req.params.id);      
